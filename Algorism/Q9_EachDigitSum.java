f0181f414a2519ab74f3605e3dd8be94865c2e4c
JaeMin1130 <ija06598@gmail.com>
2023-04-06 00:17:26 +0900

    Q9_각 자릿수의 합 구하기

--- Algorism/Q9_EachDigitSum.java
+++ Algorism/Q9_EachDigitSum.java
@@ -8,8 +8,8 @@ public class Q9_EachDigitSum {
         System.out.println("수를 입력하세요.");
         String num1 = scanner.next();
         char[] digitArr = num1.toCharArray();
-
         int digitSum1 = 0;
+
         for (int i = 0; i < digitArr.length; i++) {
             digitSum1 += Character.getNumericValue(digitArr[i]);
         }
@@ -18,12 +18,14 @@ public class Q9_EachDigitSum {
         // Teacher's answer
         System.out.println("수를 입력하세요.");
         int num2 = scanner.nextInt();
-        scanner.close();
         int digitsum2 = 0;
+        
         while (num2 > 0) {
             digitsum2 += num2 % 10;
             num2 /= 10;
         }
         System.out.println("각 자릿수의 합 = " + digitsum2);
+        
+        scanner.close();
     }
 }

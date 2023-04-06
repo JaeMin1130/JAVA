0f1eb2f399a9f6129591b8391a13be8b27251000
JaeMin1130 <ija06598@gmail.com>
2023-04-06 21:31:17 +0900

    Q10_정방행렬 출력하기

--- Algorism/Q10_Matrix.java
+++ Algorism/Q10_Matrix.java
@@ -8,14 +8,14 @@ public class Q10_Matrix {
         int num = scanner.nextInt();
         int[][] array = new int[num][num];
         int k = 0;
-        
+
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0 ; j < array.length; j++) {
+            for (int j = 0; j < array.length; j++) {
                 array[i][j] = k++;
                 System.out.print(array[i][j] + "\t");
             }
             System.out.println("\n");
-        } 
+        }
         scanner.close();
     }
 }
